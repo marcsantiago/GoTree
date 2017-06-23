9b6cae09d905e7321e6f8254742064f75d2792f5
Marc Santiago <marcanthonysanti@gmail.com>
2017-06-22 22:49:38 -0400

    updating example

--- example.go
+++ example.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"./tree"
 	// "github.com/marcsantiago/GoTree/tree"
@@ -13,31 +14,31 @@ func main() {
 	// While suming with this function is a slower then just calling GetTreeTotal()
 	// the real bottle neck is adding that many Nodes to the tree. It uses a lot of memory, so be careful when
 	// executing this funcion
-	// t := tree.NewTree()
-	// log.Println("Creating a tree with 16777216 nodes")
-	// t.GenerateRandomTreeIteratively(16777216)
-	// log.Println("Adding Nodes using the slower Sum() function")
-	// log.Println("Total:", t.Sum())
-
-	// t = tree.NewTree()
-	// t.AddIteratively(10)
-	// t.AddIteratively(100)
-	// t.AddIteratively(2)
-	// t.AddIteratively(3)
-	// fmt.Println(t.Sum()) // should be 142
-	// fmt.Println(t.GetTreeTotal())
-	// fmt.Println("edges", t.CountEdges())
-	// fmt.Printf("%+v\n", t.TreeToArray())
-	// t.PrintTree()
-
 	t := tree.NewTree()
-	t.AddIteratively(1)
-	t.AddIteratively(2)
-	t.AddIteratively(3)
-	t.AddIteratively(4)
-	t.AddIteratively(10)
-	t.AddIteratively(9)
-	t.AddIteratively(8)
+	log.Println("Creating a tree with 16777216 nodes")
+	t.GenerateRandomTreeIteratively(16777216)
+	log.Println("Adding Nodes using the slower Sum() function")
+	log.Println("Total:", t.Sum())
+
+	t = tree.NewTree()
+	t.AddIteratively(10, false)
+	t.AddIteratively(100, false)
+	t.AddIteratively(2, false)
+	t.AddIteratively(3, false)
+	fmt.Println(t.Sum()) // should be 142
+	fmt.Println(t.GetTreeTotal())
+	fmt.Println("edges", t.CountEdges())
+	fmt.Printf("%+v\n", t.TreeToArray())
+	t.PrintTree()
+
+	t = tree.NewTree()
+	t.AddIteratively(1, false)
+	t.AddIteratively(2, false)
+	t.AddIteratively(3, false)
+	t.AddIteratively(4, false)
+	t.AddIteratively(10, false)
+	t.AddIteratively(9, false)
+	t.AddIteratively(8, false)
 	t.PrintTree()
 	fmt.Println(t.GetDepth())
 	fmt.Println(t.IsBalanced())
